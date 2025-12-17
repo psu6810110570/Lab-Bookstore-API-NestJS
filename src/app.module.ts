e0e6eaaf1a5c05340a0358cdc09ece89eccf77c7
psu6810110570 <chindanai.ac@gmail.com>
2025-12-17 10:50:42 +0700

    Checkpoint_1

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BookCategory } from './book-category/entities/book-category.entity';
+import { BookCategoryModule } from './book-category/book-category.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'admin',
+      password: 'password123',
+      database: 'bookstore_dev',
+      entities: [BookCategory],
+      synchronize: true,
+    }),
+    BookCategoryModule,
+  ],
+})
+export class AppModule {}
---
+++ src/book-category/entities/book-category.entity.ts
@@ -0,0 +1,19 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity()
+export class BookCategory {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({ nullable: true })
+  description: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
